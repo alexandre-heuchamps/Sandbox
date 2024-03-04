611ba91beff2306d064ad90dabe4e7b21214e96e
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-03-04 20:42:25 +0100

    Creating __init__ file

--- Python/Blender/__init__.py
+++ Python/Blender/__init__.py
@@ -4,17 +4,22 @@ from .PROPERTY_GROUP.PlatformPanelProps import PLATFORM_PlatformPanelProps
 from .PANELS.Platform import PLATFORM_PT_Platform
 from .OPERATORS.AddPlatform import PLATFORM_OT_AddPlatform
 from .OPERATORS.DeletePlatform import PLATFORM_OT_DeletePlatform
+# import bpy
+# from PROPERTY_GROUP.Datafile import PLATFORM_DataFile
+# from PROPERTY_GROUP.PlatformPanelProps import PLATFORM_PlatformPanelProps
+# from PANELS.Platform import PLATFORM_PT_Platform
+# from OPERATORS.AddPlatform import PLATFORM_OT_AddPlatform
+# from OPERATORS.DeletePlatform import PLATFORM_OT_DeletePlatform
 
-classes = (
-            PLATFORM_DataFile,
-            PLATFORM_PlatformPanelProps,
-            PLATFORM_PT_Platform,
-            PLATFORM_OT_AddPlatform,
-            PLATFORM_OT_DeletePlatform,
-        )
+__all__ = ["PLATFORM_DataFile",
+            "PLATFORM_PlatformPanelProps",
+            "PLATFORM_PT_Platform",
+            "PLATFORM_PT_Platform",
+            "PLATFORM_OT_AddPlatform",
+            "PLATFORM_OT_DeletePlatform",]
 
 def register():
-    for my_class in classes:
+    for my_class in __all__:
         bpy.utils.register_class(my_class)
     bpy.types.Scene.eff_file = bpy.props.PointerProperty(type=PLATFORM_DataFile)
     bpy.types.Scene.platform_panel_props = bpy.props.CollectionProperty(
@@ -22,7 +27,7 @@ def register():
                                             )
 
 def unregister():
-    for my_class in classes:
+    for my_class in __all__:
         bpy.utils.unregister_class(my_class)
     del bpy.types.Scene.eff_file
 
