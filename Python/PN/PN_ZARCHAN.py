a0eb9570570608e83940261dcf04b55972a86262
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-03-05 09:17:57 +0100

    Comments explaining the code flow

--- Python/PN/PN_ZARCHAN.py
+++ Python/PN/PN_ZARCHAN.py
@@ -61,6 +61,7 @@ if __name__ == "__main__":
         else:
             H = 0.01
 
+        # Needed for update
         BETAOLD = BETA
         RT1OLD = RT1
         RT2OLD = RT2
@@ -69,4 +70,63 @@ if __name__ == "__main__":
         VM1OLD = VM1
         VM2OLD = VM2
         STEP = 1
-        FLAG = 0
+        FLAG = 0        # apply a midpoint 2nd order Runge-Kutta method if set
+
+        while(STEP <= 1):
+            if FLAG == 1:
+                STEP = 2
+                BETA = BETA + H * BETAD
+                RT1 = RT1 + H * VT1
+                RT2 = RT2 + H * VT2
+                RM1 = RM1 + H * VM1
+                RM2 = RM2 + H * VM2
+                VM1 = VM1 + H * AM1
+                VM2 = VM2 + H * AM2
+                T = T + H
+
+            RTM1 = RT1 - RM1
+            RTM2 = RT2 - RM2
+            RTM = np.sqrt(RTM1**2 + RTM2**2)
+            VTM1 = VT1 - VM1
+            VTM2 = VT2 - VM2
+            VC = -(RTM1 * VTM1 + RTM2 * VTM2) / RTM
+            XLAM = np.arctan2(RTM2, RTM1)
+            XLAMD = (RTM1 * VTM2 - RTM2 * VTM1) / (RTM**2)
+            XNC = XNP * VC * XLAMD
+            AM1 = -XNC * np.sin(XLAM)
+            AM2 = XNC * np.cos(XLAM)
+            VT1 = -VT * np.cos(BETA)
+            VT2 = VT * np.sin(BETA)
+            BETAD = XNT / VT            # target angular velocity
+            FLAG = 1                    # set to enable midpoint RK2 solver
+
+        # Propagate the states using a midpoint second-order Runge-Kutta method
+        FLAG = 0                                    # unset to disable RK solver
+        BETA = 0.5 * (BETAOLD + BETA + H * BETAD)
+        RT1 = 0.5 * (RT1OLD + RT1 + H * VT1)
+        RT2 = 0.5 * (RT2OLD + RT2 + H * VT2)
+        RM1 = 0.5 * (RM1OLD + RM1 + H * VM1)
+        RM2 = 0.5 * (RM2OLD + RM2 + H * VM2)
+        VM1 = 0.5 * (VM1OLD + VM1 + H * AM1)
+        VM2 = 0.5 * (VM2OLD + VM2 + H * AM2)
+        S = S + H
+
+        # Store data every 0.0999 seconds
+        if S > 0.09999:
+            S = 0.0
+            n = n + 1
+            ArrayT.append([T])
+            ArrayRT1.append([RT1])
+            ArrayRT2.append([RT2])
+            ArrayRM1.append([RM1])
+            ArrayRM2.append([RM2])
+            ArrayXNCG.append([XNC / 32.2])
+            ArrayRTM.append([RTM])
+
+    # Plot results
+    plt.plot(ArrayRT1, ArrayRT2, '-*', label = "Target")
+    plt.plot(ArrayRM1, ArrayRM2, '-s', label = "Missile")
+    plt.xlabel("Downrange (ft)")
+    plt.ylabel("Altitude (ft)")
+    plt.legend()
+    plt.show()
