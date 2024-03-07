f8b37965a666f4bcf131fa97d4ad6b30797e3e35
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-03-07 09:12:24 +0100

    Make class 'Body' __init__ method

--- Python/PMM/Body.py
+++ Python/PMM/Body.py
@@ -1,8 +1,24 @@
+import numpy as np
+
 class Body():
-    def __init__(self) -> None:
-        pass
+    """ Generic class to represent a 'body'. Can be a projectile or a target """
+
+    def __init__(self,
+                    m: float = 1.0,
+                    x0: np.ndarray = np.array([0.0, 0.0, 0.0]),
+                 ) -> None:
+        """ Initiate an object of type 'Body' with a given mass an initial position
+
+        Parameters
+        ----------
+        m: <class 'float'> (default: 1.0)
+            Mass of the created object
+        x0: <class 'numpy.ndarray'> (default: [0.0, 0.0, 0.0])
+            Initial position of the created object """
+        self._m = m
+        self._x0 = x0
 
 
 
 if __name__ == "__main__":
-    pass
+    b = Body()
