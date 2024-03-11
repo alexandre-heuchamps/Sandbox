a471e36432c14b1776ca107e59daecdebf007d4a
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-03-11 15:15:24 +0100

    Remove unnecessary functions

--- Python/PMM/Simulator.py
+++ Python/PMM/Simulator.py
@@ -57,68 +57,6 @@ class Simulator():
     # ==========================================================================
 
     # ==========================================================================
-    def speed_to_Ma(self, v: float, vsound: float = 330.0) -> float:
-        """ Converts a speed expressed in m/s to a speed expressed in Ma
-
-        Parameters
-        ----------
-        v: <class 'float'>
-            Speed to convert in Ma
-        vsound: <class 'float'> (default: 330.0)
-            Speed of sound
-
-        Returns
-        -------
-        _: <class 'float'>
-            Equivalent of input speed, in terms of Ma """
-        return (v / vsound)
-
-    def Ma_to_speed(self, Ma: float, vsound: float = 330.0) -> float:
-        """ Converts a speed expressed in Ma to a speed expressed in m/s
-
-        Parameters
-        ----------
-        Ma: <class 'float'>
-            Speed to convert in m/s
-        vsound: <class 'float'> (default: 330.0)
-            Speed of sound
-
-        Returns
-        -------
-        _: <class 'float'>
-            Equivalent of input speed, in terms of m/s """
-        return (Ma * vsound)
-
-    def speed_to_kph(self, v: float) -> float:
-        """ Converts a speed expressed in m/s to a speed expressed in km/h
-
-        Parameters
-        ----------
-        v: <class 'float'>
-            Speed of to convert in km/h
-
-        Returns
-        -------
-        _: <class 'float'>
-            Equivalent of input speed, in terms of km/h """
-        return (3.6 * v)
-
-    def kph_to_speed(self, v: float) -> float:
-        """ Converts a speed expressed in km/h to a speed expressed in m/s
-
-        Parameters
-        ----------
-        v: <class 'float'>
-            Speed of to convert in m/s
-
-        Returns
-        -------
-        _: <class 'float'>
-            Equivalent of input speed, in terms of m/s """
-        return (v / 3.6)
-    # ==========================================================================
-
-    # ==========================================================================
     @property
     def world(self) -> World:
         """ Get or set the world in which the simulation takes place """
@@ -133,5 +71,3 @@ class Simulator():
 
 if __name__ == "__main__":
     sim = Simulator()
-    v_mps = 330.0
-    print(f"{v_mps} m/s == {sim.speed_to_kph(v_mps)} km/h == {sim.speed_to_Ma(v_mps)} Ma")
