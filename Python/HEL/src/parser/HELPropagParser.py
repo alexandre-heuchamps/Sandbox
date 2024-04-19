ae73ff830dbd376e7108f2603b4a0ec7b8cc6a80
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-04-19 21:09:04 +0200

    Make support for list of files

--- Python/HEL/src/parser/HELPropagParser.py
+++ Python/HEL/src/parser/HELPropagParser.py
@@ -5,5 +5,5 @@ from FilePropagParser import FilePropagParser
 class HELPropagParser(FilePropagParser):
     """ Class for parsing propagation files for HEL """
 
-    def __init__(self, file: str) -> None:
-        self._file: str = file
+    def __init__(self, files: list[str]) -> None:
+        self._files: list[str] = files
