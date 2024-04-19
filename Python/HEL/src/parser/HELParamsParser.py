c28d6be76caf72d1c5e1734098ce4adf53634fb9
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-04-19 15:36:28 +0200

    Create HELParamParser class

--- Python/HEL/src/parser/HELParamsParser.py
+++ Python/HEL/src/parser/HELParamsParser.py
@@ -1,4 +1,8 @@
-class HELParamsParser:
+from .FileParamParser import FileParamParser
+
+
+
+class HELParamsParser(FileParamParser):
     """ Class for parsing params files for HEL """
 
     def __init__(self) -> None:
