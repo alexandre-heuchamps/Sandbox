1013767a9a66e0f64631195e4ba45bb5eec854ee
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-04-19 15:45:43 +0200

    Rename class

---
+++ Python/HEL/src/parser/HELParamParser.py
@@ -0,0 +1,9 @@
+from .FileParamParser import FileParamParser
+
+
+
+class HELParamParser(FileParamParser):
+    """ Class for parsing params files for HEL """
+
+    def __init__(self, file: str) -> None:
+        self._file: str = file
