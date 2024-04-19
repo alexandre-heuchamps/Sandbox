744dee68e856c0146eaf1157a6d8d6d7de1ca6bd
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-04-19 15:43:42 +0200

    Add file attribute

--- Python/HEL/src/parser/DronePropagParser.py
+++ Python/HEL/src/parser/DronePropagParser.py
@@ -1,5 +1,9 @@
-class DronePropagParser:
+from .FilePropagParser import FilePropagParser
+
+
+
+class DronePropagParser(FilePropagParser):
     """ Class for parsing propagation files for drone """
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, file: str) -> None:
+        self._file: str = file
