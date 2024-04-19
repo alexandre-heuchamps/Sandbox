0fb6509e3d721afb9729478adb2ec87858de3971
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-04-19 15:45:59 +0200

    Rename class

---
+++ Python/HEL/src/parser/DroneParamParser.py
@@ -0,0 +1,9 @@
+from .FileParamParser import FileParamParser
+
+
+
+class DroneParamParser(FileParamParser):
+    """ Class for parsing params files for drone """
+
+    def __init__(self, file: str) -> None:
+        self._file: str = file
