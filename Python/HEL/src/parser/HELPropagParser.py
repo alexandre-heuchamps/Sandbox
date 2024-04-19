9b19a132e58466a892526bcf14c98f8e204cdeb2
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-04-19 15:36:44 +0200

    Create HELPropagParser class

--- Python/HEL/src/parser/HELPropagParser.py
+++ Python/HEL/src/parser/HELPropagParser.py
@@ -1,4 +1,8 @@
-class HELPropagParser:
+from .FilePropagParser import FilePropagParser
+
+
+
+class HELPropagParser(FilePropagParser):
     """ Class for parsing propagation files for HEL """
 
     def __init__(self) -> None:
