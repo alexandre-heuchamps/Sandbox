11aaa600416a61d012d276166be0cbcaf1c156ce
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-04-19 21:08:34 +0200

    Make support for list of files

--- Python/HEL/src/parser/HELParamParser.py
+++ Python/HEL/src/parser/HELParamParser.py
@@ -5,5 +5,5 @@ from FileParamParser import FileParamParser
 class HELParamParser(FileParamParser):
     """ Class for parsing params files for HEL """
 
-    def __init__(self, file: str) -> None:
-        self._file: str = file
+    def __init__(self, files: list[str]) -> None:
+        self._files: list[str] = files
