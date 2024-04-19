33fb368015e4f7590c4fa469fa32a31b51246d8a
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-04-19 15:44:08 +0200

    Add file attribute

--- Python/HEL/src/parser/HELPropagParser.py
+++ Python/HEL/src/parser/HELPropagParser.py
@@ -5,5 +5,5 @@ from .FilePropagParser import FilePropagParser
 class HELPropagParser(FilePropagParser):
     """ Class for parsing propagation files for HEL """
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, file: str) -> None:
+        self._file: str = file
