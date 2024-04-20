95159d45c9011147e1a244e708c52797f57a6522
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-04-20 20:07:56 +0200

    Make relative imports, add world dependence, fix annotation, raise exception if not implemented functions/methods are called

--- Python/HEL/src/body/Simulator.py
+++ Python/HEL/src/body/Simulator.py
@@ -1,23 +1,23 @@
-from typing import List, Tuple
-from CLParser import CLParser
-from HELParser import HELParser
-from DroneParser import DroneParser
-from HEL import HEL
-from Drone import Drone
+from ..parser.CLParser import CLParser
+from .HEL import HEL
+from .Drone import Drone
+from .World import World
 
 class Simulator:
     def __init__(self,
                     cl_parser: CLParser,
+                    world: World,
                     dt: float = 1e-3,
                     t_max: float = 1e3,
                 ) -> None:
         self._cl_parser: CLParser = cl_parser
+        self._world: World = world
         self._dt: float = dt
         self._t_max: float = t_max
-        self._HELs: List[HEL] = []
-        self._Drones: List[Drone] = []
+        self._HELs: list[HEL] = []
+        self._Drones: list[Drone] = []
 
-        if self.cl_parser.has_files():
+        if self.cl_parser.get_nopt_files():
             self.initialisation_with_file()
         else:
             self.initialisation_without_file()
@@ -54,13 +54,13 @@ class Simulator:
 
     # ==========================================================================
     @property
-    def HELs(self) -> List[HEL]:
+    def HELs(self) -> list[HEL]:
         return self._HELs
     # ==========================================================================
 
     # ==========================================================================
     @property
-    def Drones(self) -> List[Drone]:
+    def Drones(self) -> list[Drone]:
         return self._Drones
     # ==========================================================================
 
@@ -71,48 +71,22 @@ class Simulator:
 
     # ==========================================================================
     def initialisation_with_file(self) -> None:
-        HEL_param_file = self._cl_parser.get_HEL_param_files()
-        Drone_param_file = self._cl_parser.get_Drone_param_files()
-        HEL_parser = HELParser(HEL_file = HEL_param_file[-1])
-        nHEL = HEL_parser.get_nHEL(cl_parser = self._cl_parser)[0, 0]
-        drone_parser_params = DroneParser(drone_file = Drone_param_file[-1])
-        nDrone = drone_parser_params.get_sheet_number()
-
-        HEL_names = HEL_parser.get_HEL_names()
-        HEL_init_pos = HEL_parser.get_HEL_init_pos()
-        HEL_lambdas = HEL_parser.get_HEL_wavelengths()
-        HEL_Ps = HEL_parser.get_HEL_Ps()
-        HEL_M2s = HEL_parser.get_HEL_M2s()
-        HEL_Js = HEL_parser.get_HEL_jitters()
-        HEL_w0s = HEL_parser.get_HEL_w0s()
-        HEL_modes = HEL_parser.get_HEL_modes()
-        for i in range(nHEL):
-            self._HELs.append(
-                                HEL(name = HEL_names[i],
-                                    wavelength = HEL_lambdas[i],
-                                    P = HEL_Ps[i],
-                                    M2 = HEL_M2s[i],
-                                    J = HEL_Js[i],
-                                    w0 = HEL_w0s[i],
-                                    mode = HEL_modes[i],
-                                    x0 = HEL_init_pos[i],
-                                )
-                            )
-
-        for i in range(nDrone):
-            self._Drones.append(Drone())
+        raise NotImplementedError("Simulation initialisation with input file under construction. Stay tuned!")
     # ==========================================================================
 
     # ==========================================================================
     def run(self) -> None:
-        print(self._HELs)
-        print(self._Drones)
+        raise NotImplementedError("Simulation run under construction. Stay tuned!")
     # ==========================================================================
 
 
 
 
 if __name__ == "__main__":
+    """ Note:
+    Run the line
+    python -m src.body.Simulator -nHEL 2 -nDrone 3 --file-param-HEL data\Effectors.xlsx --file-param-drone data\Drones_Params.xlsx --file-propag-drone data\Drones_Propagation.xlsx
+    from HEL/ """
     cl_parser = CLParser()
-    sim = Simulator(cl_parser = cl_parser)
-    sim.run()
+    world = World()
+    sim = Simulator(cl_parser = cl_parser, world = world)
