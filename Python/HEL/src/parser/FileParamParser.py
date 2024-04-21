0ef521f29b1c7d3fda9b7af164b1dcbe05204db4
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-04-21 18:19:05 +0200

    Add capability to extract all the names present in the sheets

--- Python/HEL/src/parser/FileParamParser.py
+++ Python/HEL/src/parser/FileParamParser.py
@@ -9,3 +9,10 @@ class FileParamParser(FileParser):
 
     def __init__(self, files: list[str]) -> None:
         super().__init__(files = files)
+
+    def get_names_in_files(self) -> list[list[str]]:
+        names = []
+        for df in super().get_dfs():
+            for data in df:
+                names.append(list(data["Name"]))
+        return names
