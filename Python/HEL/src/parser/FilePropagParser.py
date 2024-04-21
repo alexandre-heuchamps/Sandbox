95d0be4ebd70066e54419e93950708518407f2f1
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-04-21 22:10:33 +0200

    Add column data extraction capability

--- Python/HEL/src/parser/FilePropagParser.py
+++ Python/HEL/src/parser/FilePropagParser.py
@@ -9,3 +9,16 @@ class FilePropagParser(FileParser):
 
     def __init__(self, files: list[str]) -> None:
         super().__init__(files = files)
+
+    def get_column_content(self, column_name: str) -> list[list[str]]:
+        """ Function returning a list of list of strings corresponding to the
+        data of the requested column """
+        data = []
+        for df in super().get_dfs():
+            for d in df:
+                data.append(list(d[column_name]))
+        return data
+
+    def get_times_in_files(self) -> list[list[str]]:
+        """ Function returning all the names contained in the files """
+        return self.get_column_content(column_name = "Time")
