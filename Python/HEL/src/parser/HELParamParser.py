d115942bb794acf5ab2892cc8a10461c460e25b5
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-04-22 14:31:08 +0200

    Add M2 extraction capability

--- Python/HEL/src/parser/HELParamParser.py
+++ Python/HEL/src/parser/HELParamParser.py
@@ -25,3 +25,12 @@ class HELParamParser(FileParamParser):
             P_list.append([float(p) for p in P])
         return P_list
     # ==========================================================================
+
+    # ==========================================================================
+    def get_M2s(self) -> list[float]:
+        M2s = super().get_column_content("Power")
+        M2_list = []
+        for M2 in M2s:
+            M2_list.append([float(m2) for m2 in M2])
+        return M2_list
+    # ==========================================================================
