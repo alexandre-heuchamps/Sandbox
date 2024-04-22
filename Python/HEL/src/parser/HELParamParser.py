444a17086a6aae3a95b835f8bbd51c93b2c84aed
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-04-22 14:23:06 +0200

    Add wavelength extraction capability

--- Python/HEL/src/parser/HELParamParser.py
+++ Python/HEL/src/parser/HELParamParser.py
@@ -7,3 +7,12 @@ class HELParamParser(FileParamParser):
 
     def __init__(self, files: list[str]) -> None:
         super().__init__(files = files)
+
+    # ==========================================================================
+    def get_wavelengths(self) -> list[float]:
+        l = super().get_column_content("wavelength")
+        lmbda_list = []
+        for lmbda in l:
+            lmbda_list.append([float(ll) for ll in lmbda])
+        return lmbda_list
+    # ==========================================================================
