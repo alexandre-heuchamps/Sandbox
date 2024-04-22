4a1308540b90808e3a9373c2d187fb5f88d94456
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-04-22 11:36:56 +0200

    Add name consistency check capabilities

--- Python/HEL/src/checker/ConsistencyCheckerNames.py
+++ Python/HEL/src/checker/ConsistencyCheckerNames.py
@@ -1,10 +1,4 @@
-from src.body.World import World
-from src.body.HEL import HEL
-from src.body.Drone import Drone
-
-
-
-class ConsistencyCheckerWorld:
+class ConsistencyCheckerNames:
     """ A class to check if sheet names are consistent with names in column """
 
     def __init__(self,
@@ -26,9 +20,29 @@ class ConsistencyCheckerWorld:
         return self._names_col
     # ==========================================================================
 
+    # ==========================================================================
+    def are_lengths_consistent(self) -> bool:
+        """ Function checking if the number of sheets match the number of names """
+        return True if len(self._names_sheets) == len(self._names_col) else False
+    # ==========================================================================
+
+    # ==========================================================================
+    def are_names_consistency(self) -> bool:
+        if not self.are_lengths_consistent():
+            raise ValueError("Name inconsistency between sheets and column names")
+        else:
+            snames = self._names_sheets
+            cnames = self._names_col
+            all_ok = all(sn == cn for sn, cn in zip(snames, cnames))
+            return True if all_ok else False
+    # ==========================================================================
+
 
 
 
 
 if __name__ == "__main__":
-    pass
+    nsheet = ["DJI1", "R"]
+    ncol = ["DJI1", "R"]
+    c_check = ConsistencyCheckerNames(names_sheets = nsheet, names_col = ncol)
+    print(f"Are names consistent? {c_check.are_names_consistency()}")
