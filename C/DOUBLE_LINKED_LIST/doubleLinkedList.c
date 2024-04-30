47427c2885dbaee71e25ecba36e3f4ff641a6e9d
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-04-30 18:29:30 +0200

    Make code more readable

--- C/DOUBLE_LINKED_LIST/doubleLinkedList.c
+++ C/DOUBLE_LINKED_LIST/doubleLinkedList.c
@@ -11,7 +11,8 @@
 
 
 // Define the Node data structure
-typedef struct Node {
+typedef struct Node
+{
     int data;
     struct Node* next;
     struct Node* prev;
@@ -27,13 +28,16 @@ typedef struct Node {
 
 
 // Function to insert a node at the start of the linked list
-void insert_start(Node** headRef, int data) {
+void
+insert_start(Node** headRef, int data)
+{
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->data = data;
     newNode->next = *headRef;
     newNode->prev = NULL;
 
-    if (*headRef != NULL) {
+    if (*headRef != NULL)
+    {
         (*headRef)->prev = newNode;
     }
 
@@ -50,15 +54,20 @@ void insert_start(Node** headRef, int data) {
 
 
 // Function to insert a node at the end of the linked list
-void insert_end(Node** headRef, int data) {
+void
+insert_end(Node** headRef, int data)
+{
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->data = data;
     newNode->next = NULL;
 
-    if (*headRef == NULL) {
+    if (*headRef == NULL)
+    {
         newNode->prev = NULL;
         *headRef = newNode;
-    } else {
+    }
+    else
+    {
         Node* last = *headRef;
         while (last->next != NULL) {
             last = last->next;
@@ -78,15 +87,19 @@ void insert_end(Node** headRef, int data) {
 
 
 // Function to delete the first node of the linked list
-void delete_start(Node** headRef) {
-    if (*headRef == NULL) {
+void
+delete_start(Node** headRef)
+{
+    if (*headRef == NULL)
+    {
         return;
     }
 
     Node* nextNode = (*headRef)->next;
     free(*headRef);
 
-    if (nextNode != NULL) {
+    if (nextNode != NULL)
+    {
         nextNode->prev = NULL;
     }
 
@@ -103,19 +116,26 @@ void delete_start(Node** headRef) {
 
 
 // Function to delete the last node of the linked list
-void delete_end(Node** headRef) {
-    if (*headRef == NULL) {
+void
+delete_end(Node** headRef)
+{
+    if (*headRef == NULL)
+    {
         return;
     }
 
     Node* last = *headRef;
-    while (last->next != NULL) {
+    while (last->next != NULL)
+    {
         last = last->next;
     }
 
-    if (last->prev != NULL) {
+    if (last->prev != NULL)
+    {
         last->prev->next = NULL;
-    } else {
+    }
+    else
+    {
         *headRef = NULL;
     }
 
@@ -132,27 +152,36 @@ void delete_end(Node** headRef) {
 
 
 // Function to delete a node at a specific position in the linked list
-void delete_position(Node** headRef, unsigned int position) {
-    if (*headRef == NULL) {
+void
+delete_position(Node** headRef, unsigned int position)
+{
+    if (*headRef == NULL)
+    {
         return;
     }
 
     Node* temp = *headRef;
     unsigned int count = 0;
 
-    while (temp != NULL && count < position) {
+    while (temp != NULL && count < position)
+    {
         temp = temp->next;
         count++;
     }
 
-    if (temp != NULL) {
-        if (temp->prev != NULL) {
+    if (temp != NULL)
+    {
+        if (temp->prev != NULL)
+        {
             temp->prev->next = temp->next;
-        } else {
+        }
+        else
+        {
             *headRef = temp->next;
         }
 
-        if (temp->next != NULL) {
+        if (temp->next != NULL)
+        {
             temp->next->prev = temp->prev;
         }
 
@@ -170,8 +199,11 @@ void delete_position(Node** headRef, unsigned int position) {
 
 
 // Function to print the linked list
-void printList(Node* node) {
-    while (node != NULL) {
+void
+printList(Node* node)
+{
+    while (node != NULL)
+    {
         printf("%d ", node->data);
         node = node->next;
     }
@@ -187,11 +219,14 @@ void printList(Node* node) {
 
 
 // Function to free the linked list
-void freeLinkedList(Node** headRef) {
+void
+freeLinkedList(Node** headRef)
+{
     Node* current = *headRef;
     Node* next;
 
-    while (current != NULL) {
+    while (current != NULL)
+    {
         next = current->next;
         free(current);
         current = next;
@@ -209,7 +244,9 @@ void freeLinkedList(Node** headRef) {
 
 
 
-int main() {
+int
+main()
+{
     Node* head = NULL;
 
     insert_start(&head, 1);  // Insert 1 at the start
