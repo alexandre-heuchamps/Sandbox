f5f3994faedc8c0f490e5ad56bd59fba9a42ffac
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-04-30 20:50:59 +0200

    Add rotate_point method

--- C/POLYGON_NEW/main.c
+++ C/POLYGON_NEW/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "point.h"
 
+#define PI 3.14159265358979323846
+
 int main()
 {
     point *pt = alloc_point();
@@ -15,6 +17,11 @@ int main()
     y = 5.48;
     init_point(pt, x, y);
     print_point(pt);
+    point *pt_rot = alloc_point();
+    init_point(pt_rot, 0.0, 0.0);
+    rotate_point(pt, pt_rot, 0.25 * PI);
+    print_point(pt);
+    free(pt_rot);
     free_point(pt);
     return 0;
 }
--- C/POLYGON_NEW/point.c
+++ C/POLYGON_NEW/point.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include "point.h"
 
 
@@ -147,6 +148,36 @@ void *print_point(const point *pt)
     else
     {
         printf("(x, y) = (%lf, %lf)\n", get_x_point(pt), get_y_point(pt));
+    }
+    return NULL;
+}
+
+
+
+
+
+void *rotate_point(point *pt, const point *origin, float ang)
+{
+    if(NULL == pt || NULL == origin)
+    {
+        fprintf(stderr, "Cannot rotate point.\n");
+    }
+    else
+    {
+        float x_origin = get_x_point(origin);
+        float y_origin = get_y_point(origin);
+
+        float x_pt = get_x_point(pt);
+        float y_pt = get_y_point(pt);
+
+        float x_to_origin = x_pt - x_origin;
+        float y_to_origin = y_pt - y_origin;
+
+        float x_pt_rot = x_to_origin * cos(ang) - y_to_origin * sin(ang);
+        float y_pt_rot = x_to_origin * sin(ang) + y_to_origin * cos(ang);
+
+        set_x_point(pt, x_pt_rot + x_origin);
+        set_y_point(pt, y_pt_rot + y_origin);
     }
     return NULL;
 }
