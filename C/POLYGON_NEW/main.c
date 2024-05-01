33d9097fd3776f04db3c48dca5f25ca8eb7a3726
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-05-01 18:17:44 +0200

    Remove useless tests

--- C/POLYGON_NEW/main.c
+++ C/POLYGON_NEW/main.c
@@ -12,25 +12,10 @@ int main()
     float x = 0.0;
     float y = 2.3;
     init_point(pt, x, y);
-    printf("Initial point: ");
-    print_point(pt);
-
-    point *pt_rot = alloc_point();
-    init_point(pt_rot, 0.0, 0.0);
-    rotate_point(pt, pt_rot, 0.25 * PI);
-    printf("Rotated point: ");
-    print_point(pt);
 
     vector *v = alloc_vector();
-    set_origin_vector(v, pt_rot);
-    set_end_vector(v, pt);
-    print_vector(v);
-
-    translate_point(pt, v);
-    print_point(pt);
 
     free_vector(v);
-    free_point(pt_rot);
     free_point(pt);
     return 0;
 }
