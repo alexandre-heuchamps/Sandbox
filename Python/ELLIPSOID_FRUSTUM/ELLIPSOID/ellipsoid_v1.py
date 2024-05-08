a19c5b700452cc579ab02d17a0341eb11f5f9b9a
Alexandre HEUCHAMPS <alexandre.heuchamps@dymasec.be>
2024-05-08 09:46:24 +0200

    Add properties and corresponding setters

--- Python/ELLIPSOID_FRUSTUM/ELLIPSOID/ellipsoid_v1.py
+++ Python/ELLIPSOID_FRUSTUM/ELLIPSOID/ellipsoid_v1.py
@@ -13,3 +13,33 @@ class Ellipsoid:
         self._c: np.ndarray = np.array(c)
         self._Sigma: np.ndarray = np.diag([1/lx**2, 1/ly**2, 1/lz**2])
         self._U: np.ndarray = np.array(U)
+
+    # ==========================================================================
+    @property
+    def c(self) -> np.ndarray:
+        return self._c
+
+    @c.setter
+    def c(self, c: list[float]) -> None:
+        self._c = np.array(c)
+    # ==========================================================================
+
+    # ==========================================================================
+    @property
+    def Sigma(self) -> np.ndarray:
+        return self._Sigma
+
+    @Sigma.setter
+    def Sigma(self, Sigma: list[float]) -> None:
+        self._Sigma = np.array(Sigma)
+    # ==========================================================================
+
+    # ==========================================================================
+    @property
+    def U(self) -> np.ndarray:
+        return self._U
+
+    @U.setter
+    def U(self, U: list[float]) -> None:
+        self._U = np.array(U)
+    # ==========================================================================
